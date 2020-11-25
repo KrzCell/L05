2bc1f963ad70524aff53d1baff2b7967b1b56226
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-11-25 13:48:34 +0100

    koniec5A

--- L05.cpp
+++ L05.cpp
@@ -11,23 +11,25 @@ int main(void)
 	constexpr int SIZE = 5;
 	int tab[SIZE][SIZE] = {};
 	int i;
+	bool tab2[SIZE * SIZE + 1] = {};
+	bool rozne = true;
 
 	// Element początkowy:
 	int w = 0, k = SIZE/2;
 	tab[w][k] = 1;
 
 	//Następne elementy:
-	for (i = 0; i < SIZE * SIZE; i++)
+	for (i = 2; i <= SIZE * SIZE; i++)
 	{
 		//Przesuń współrzędne
-		w ++, k --;
+		w --, k ++;
 		//Sprawdź i skoryguj naruszenie granic tablicy:
 		if (w < 0)
 			w += SIZE;
 		if (k >= SIZE)
 			k -= SIZE;
 		//Sprawdź, czy element tablicy był już zapisany:
-		if (tab[w][k] != 0)
+		if (tab[w][k])
 		{
 			//Przesuń współrzędne i sprawdź granice:
 			w += 2;
@@ -51,13 +53,31 @@ int main(void)
 		cout << endl;
 	}
 
+	for (i = 0; i < SIZE; i += 1)
+	{
+		for (int y = 0; y < SIZE; y += 1)
+		{
+			int zm = tab[i][y];
+			if (tab2[zm])
+				rozne = false;
+			else
+				tab2[zm] = true;
+
+		}
+		
+	}
+	if (rozne)
+		cout << "Liczby nie powtarzaja sie" << endl;
+	else
+		cout << "Liczby powtarzaja sie" << endl;
+
 }
 #endif
 
 #ifdef ZadanieB
 int main()
 {
-	// TU ROZWIĄZUJ ZADANIE B:
+	
 }
 #endif
 
